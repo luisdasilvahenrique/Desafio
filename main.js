5c8e483a0ef81dffc4d899d0ed72670ce13b446f
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2021-12-12 22:09:33 -0300

    desasfio: header

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+const btnMobile = document.getElementById('btn-mobile');
+
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault() 
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active')
+  event.currentTarget.setAttribute('aria-expanded', active)
+  if(active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar menu')
+  }else{
+    event.currentTarget.setAttribute('aria-label', 'Abrir menu')
+  } 
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
